e96b28e8246dc855265ee77a47480575653f3932
MuhammadSufian98 <sufianhassan98@gmail.com>
2025-05-12 08:31:13 +0500

    `Updated dependencies and code structure in App.js and package files`

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaView } from "react-native";
 import Header from "./components/header";
 import MainBody from "./components/mainBody";
 import LiveFeed from "./components/liveFeed";
@@ -10,14 +11,17 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Header />
-      <Stack.Navigator
-        initialRouteName="Main"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Main" component={MainBody} />
-        <Stack.Screen name="liveFeed" component={LiveFeed} />
-      </Stack.Navigator>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Header />
+
+        <Stack.Navigator
+          initialRouteName="Main"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Main" component={MainBody} />
+          <Stack.Screen name="liveFeed" component={LiveFeed} />
+        </Stack.Navigator>
+      </SafeAreaView>
     </NavigationContainer>
   );
 }
